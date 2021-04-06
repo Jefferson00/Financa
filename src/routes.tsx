0c9de38877b06470eed2f66cc06958a5ce1e4f95
Jefferson00 <jeffersonts00@gmail.com>
2021-04-06 16:41:49 -0300

    jncdcnso

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
+import { createDrawerNavigator, DrawerContentScrollView,
+    DrawerItem,
+    DrawerItemList, } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
 
 const {Navigator, Screen} = createStackNavigator();
 
@@ -9,16 +14,31 @@ import Main from './pages/main'
 import Entries from './pages/entries'
 import NewEntries from './pages/newEntries'
 import NotificationsScreen from './pages/notifications'
+import { Linking } from 'react-native';
+
+function CustomDrawerContent(props: any) {
+    return (
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        <DrawerItem
+        label="Help"
+        onPress={() => Linking.openURL('https://mywebsite.com/help')}
+      />
+      </DrawerContentScrollView>
+    );
+  }
 
 export default function Routes(){
+    
     return(
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown:false}}>
+         
+            <Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}>
                 <Screen name="Main" component={Main}/>
                 <Screen name="Entries" component={Entries}/>
                 <Screen name="NewEntries" component={NewEntries}/>
                 <Screen name="Notifications" component={NotificationsScreen}/>
-            </Navigator>
+            </Drawer.Navigator>
         </NavigationContainer>
     )
 }
